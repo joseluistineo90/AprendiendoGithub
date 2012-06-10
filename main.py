a966ce456cd418a5887c33d4717d4522038f2659
Martin Algañaraz <idcmardelplata@gmail.com>
2012-06-10 19:06:57 -0300

    Update master

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 
+def salir():
+    print 'Metodo escrito desde github'
+
 def iniciar():
     print 'Iniciando Python'
 
 if __name__=='__main__':
     iniciar()
+    salir()
 
 
